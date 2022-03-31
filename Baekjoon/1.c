6218444f3bc9d2e9c49b06d79bbd75755354d1cf
your_name <tjsxodnr2348@gmail.com>
2022-03-31 12:15:21 +0900

    초기 커밋.

---
+++ Baekjoon/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0, j = 0;
+	int sum = 0;   //변수를 초기화 이유는,.,>?
+
+	printf("Input Two integers\n");
+
+	scanf("%d%d", &i, &j);
+
+	sum = i + j;
+
+	printf("Your numbers are %d plus %d,so %d\n", i, j, sum);
+
+	return 0;
+}
+
+
+// & 앤퍼샌드  <-- 변수의 주소를 알려줌.
